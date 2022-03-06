4db35ae513b109b518492e10c970c51faaad4096
Felipe Cândido <felipe.candido8@gmail.com>
2022-03-06 16:31:33 -0300

    Ajustando Redirecionamento para Roda Admin

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::redirect('/', '/admin', 301);
 
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'as' => 'admin.'], function () {
 
